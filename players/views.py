a671b1faa2d8188edfc1894181767a9fa83cb191
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-08-27 20:06:47 -0300

    templates players add

--- players/views.py
+++ players/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Player
 
 # Create your views here.
+
+class PlayerListView(ListView):
+    """
+    Vista para mostrar la lista de todos los jugadores activos.
+    """
+    model = Player
+    template_name = 'players/player_list.html'
+    context_object_name = 'players'
+
+    def get_queryset(self):
+        # Nos aseguramos de mostrar solo los jugadores marcados como activos.
+        return Player.objects.filter(is_active=True).order_by('last_name')
+
+class PlayerDetailView(DetailView):
+    """
+    Vista para mostrar los detalles de un único jugador.
+    """
+    model = Player
+    template_name = 'players/player_detail.html'
+    context_object_name = 'player'
--- web_club/urls.py
+++ web_club/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for web_club project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # ¡Asegúrate de importar 'include'!
+
+# Necesario para servir archivos de medios (fotos) en desarrollo
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('players/', include('players.urls')), # Conecta las URLs de nuestra app
 ]
+
+# Esta línea es crucial para que se vean las fotos de los jugadores
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
