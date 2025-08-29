ceb7ca44e5e541d9435802b97edef55f5f5992cc
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-08-29 15:09:13 -0300

    hero add

--- web_club/settings.py
+++ web_club/settings.py
@@ -29,7 +29,6 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -109,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
