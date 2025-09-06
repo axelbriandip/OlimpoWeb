3497b88154657f4449cc7011dd414e17948b68ac
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-09-06 15:16:49 -0300

    dashboard member add

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_home, name='dashboard_home'),
+    path('members/', views.member_list_view, name='dashboard_member_list'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from members.models import Profile
+from django.db import models
 
 # Esta función comprueba si el usuario es un superusuario.
 def is_superuser(user):
@@ -15,3 +17,24 @@ def dashboard_home(request):
     # Más adelante, aquí obtendremos los datos (pagos pendientes, etc.).
     context = {}
     return render(request, 'dashboard/dashboard.html', context)
+
+@user_passes_test(is_superuser)
+def member_list_view(request):
+    """
+    Muestra una lista de todos los socios con opciones de búsqueda y filtro.
+    """
+    profiles = Profile.objects.select_related('user').all()
+
+    # Lógica de Búsqueda
+    query = request.GET.get('q')
+    if query:
+        profiles = profiles.filter(
+            models.Q(user__first_name__icontains=query) |
+            models.Q(user__last_name__icontains=query) |
+            models.Q(dni__icontains=query)
+        ).distinct()
+
+    context = {
+        'profiles': profiles,
+    }
+    return render(request, 'dashboard/member_list.html', context)
